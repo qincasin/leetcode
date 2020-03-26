20075fbe303b58296aa08cb22c4a791fb1c617f5
casin <qincasin@163.com>
2020-03-26 19:45:10 +0800

    feat: MergeTwoSortedLists

--- src/main/java/com/qjx/leetcode/list/MergeTwoSortedLists.java
+++ src/main/java/com/qjx/leetcode/list/MergeTwoSortedLists.java
@@ -4,49 +4,82 @@ import com.qjx.leetcode.common.ListNode;
 
 /**
  * 21. Merge Two Sorted Lists
- *Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
- *
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ * <p>
  * Example:
- *
+ * <p>
  * Input: 1->2->4, 1->3->4
  * Output: 1->1->2->3->4->4
+ *
  * @author casin
  * @create 2018-10-16 12:48
  **/
 
 public class MergeTwoSortedLists {
 
+    public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        //dummy list，新建一个链表，然后两个链表中从头各取一个元素进行比较，小的写入新链表，直到结束，返回dummy.next
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+
+        head.next = (l1 == null) ? l2 : l1;
+
+        return dummy.next;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        System.out.println(mergeTwoLists2(l1, l2));
+
+    }
+
 
     /**
      * dummy list，新建一个链表，然后两个链表中从头各取一个元素进行比较，小的写入新链表，直到结束，返回dummy.next。
+     *
      * @param l1
      * @param l2
      * @return
      */
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode flag = new ListNode(0);
-        ListNode firstFlag  = flag;
-        while (l1!=null&&l2!=null){
-            if(l1.val<l2.val){
-                flag.next=l1;
-                l1=l1.next;
-            }else {
-                flag.next=l2;
-                l2=l2.next;
+        ListNode firstFlag = flag;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                flag.next = l1;
+                l1 = l1.next;
+            } else {
+                flag.next = l2;
+                l2 = l2.next;
             }
-            flag=flag.next;
+            flag = flag.next;
         }
         flag.next = l1 != null ? l1 : l2;
         return firstFlag.next;
     }
 
-    public static void main(String[] args) {
-        ListNode l3 = new ListNode(1);
-        ListNode l4 = new ListNode(2);
-        System.out.println(mergeTwoLists(l3,l4));
+//    public static void main(String[] args) {
+//        ListNode l3 = new ListNode(1);
+//        ListNode l4 = new ListNode(2);
+//        System.out.println(mergeTwoLists(l3, l4));
+//
+//    }
 
-    }
 
-
-
-    }
+}
