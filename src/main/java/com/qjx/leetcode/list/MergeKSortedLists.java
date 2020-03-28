d313f150012fa8fbda99c8058cb3a0893da1800e
casin <qincasin@163.com>
2020-03-28 21:53:10 +0800

    feat: MergeKSortedLists

---
+++ src/main/java/com/qjx/leetcode/list/MergeKSortedLists.java
@@ -0,0 +1,116 @@
+package com.qjx.leetcode.list;
+
+import com.qjx.leetcode.common.ListNode;
+
+import java.util.Arrays;
+
+/**
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * Created by qincasin on 2020/3/26.
+ */
+public class MergeKSortedLists {
+
+    /**
+     * 分治思想
+     * time O(nlogk) where k is the number of linked lists   n is element
+     * space O(n)
+     * @param lists
+     * @return
+     */
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return sort(lists, 0, lists.length - 1);
+    }
+    /**
+     * 二分法
+     */
+    private static ListNode sort(ListNode[] lists, int low, int high) {
+        if (low >= high) {
+            return lists[low];
+        }
+        int mid = (high - low) / 2 + low;
+        ListNode l1 = sort(lists, low, mid);
+        ListNode l2 = sort(lists, mid + 1, high);
+        return mergeKLists2(l1, l2);
+    }
+
+    /**
+     * 同21. Merge Two Sorted Lists
+     */
+    private static ListNode mergeKLists2(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            } else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        head.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+
+    /**
+     * 递归merge
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    private static ListNode mergeKLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeKLists(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeKLists(l1, l2.next);
+        return l2;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        n1.next = new ListNode(4);
+        n1.next.next = new ListNode(5);
+
+        ListNode n2 = new ListNode(1);
+        n2.next = new ListNode(3);
+        n2.next.next = new ListNode(4);
+
+        ListNode n3 = new ListNode(2);
+        n3.next = new ListNode(6);
+
+        ListNode[] listNodes = new ListNode[3];
+        listNodes[0] = n1;
+        listNodes[1] = n2;
+        listNodes[2] = n3;
+
+        System.out.println(Arrays.toString(listNodes));
+
+        ListNode listNode = mergeKLists(listNodes);
+        System.out.println(listNode);
+
+
+    }
+}
--- src/main/java/com/qjx/leetcode/list/MergeTwoSortedLists.java
+++ src/main/java/com/qjx/leetcode/list/MergeTwoSortedLists.java
@@ -38,6 +38,28 @@ public class MergeTwoSortedLists {
 
     }
 
+    /**
+     * 递归方式 merge 两个 linked list
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists3(l1.next, l2);
+            return l1;
+        }
+        l2.next = mergeTwoLists3(l1, l2.next);
+        return l2;
+    }
+
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
@@ -45,7 +67,7 @@ public class MergeTwoSortedLists {
         ListNode l2 = new ListNode(2);
         l2.next = new ListNode(3);
         l2.next.next = new ListNode(4);
-        System.out.println(mergeTwoLists2(l1, l2));
+        System.out.println(mergeTwoLists3(l1, l2));
 
     }
 
